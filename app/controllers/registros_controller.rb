a18c5236aa1b8e58dce6a524ce4451a2b8edf4aa
Leonardo Placencia <placencia.leo@gmail.com>
2018-07-21 10:22:02 -0400

    Rutas completas, lista de registros completa

--- app/controllers/registros_controller.rb
+++ app/controllers/registros_controller.rb
@@ -1,10 +1,63 @@
 class RegistrosController < ApplicationController
     # Formulario para crear un registro
-    def registrar
+    def index
+        @reg = Registro.all
+    end
+    def admin
+        @reg = Registro.all
+    end
+    def new
+        @reg = Registro.new
         @nas = Nascore.all
     end
-    # Ver registros
-    def ver_registros
-        @registros = Registro.all
+    def create
+        s
+        count = nas_params['cod']
+        if Nascore.where("cod = "+count ).length > 0
+            tipo = 'multiple'
+        else
+            tipo = 'simple'
+        end
+        @nas = Nascore.new(cod: nas_params['cod'],description: nas_params['description'],score: nas_params['score'],tipo: tipo)
+        
+        if @nas.save
+            redirect_to :action => "index"
+        else
+            render :new
+        end
+    end
+    def edit
+        @nas = Nascore.find(params[:id])
+        @nascores = Nascore.select(:cod).distinct
+        if Nascore.maximum('cod').nil?
+            @max = 1
+        else
+            @max = 1 + Nascore.maximum('cod')
+        end
+    end
+    def update
+        @nas = Nascore.find(params[:id])
+        count = nas_params['cod']
+        if Nascore.where("cod = "+count+" and id !="+params[:id]  ).length > 0
+            tipo = 'multiple'
+        else 
+            tipo = 'simple'
+        end
+        if @nas.update(cod: nas_params['cod'],description: nas_params['description'],score: nas_params['score'],tipo: tipo)
+          redirect_to :action => "index"
+        else
+          render :edit
+        end
+    end
+    def destroy
+    registro = Registro.find(params[:id])
+    registro.destroy
+    redirect_to :action => "index"
+    end
+    
+    private
+    def reg_params
+        
+        params.require(:nascore).permit(:cod, :description, :score, :tipo)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :nascores, except: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/', to: 'registros#registrar', as: "home"
-
+  get '/', to: 'registros#new', as: "home"
+  get '/registro/admin', to: 'registros#admin', as: "admin"
 
 end
